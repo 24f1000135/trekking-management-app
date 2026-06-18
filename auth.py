90c43daf40e0319ed659ea5aa475db56bd9c1b6c
24f1000135 <vanshita.gautam29@gmail.com>
2026-06-18 15:55:13 +0530

    TMA-CM-2: Implemented trekker registration and login

---
+++ auth.py
@@ -0,0 +1,84 @@
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth= Blueprint("auth", __name__)
+
+@auth.route("/")
+def base():
+    return render_template("base.html")
+
+@auth.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("Email not registered.", "error")
+            return redirect(url_for("auth.login"))
+
+        if not check_password_hash(user.password, password):
+            flash("Incorrect Password", "error")
+            return redirect(url_for("auth.login"))
+
+        session['user_id'] = user.id
+        session['name'] = user.name
+        session['role'] = user.role
+
+        flash(f"Welcome, {user.name}!", "success")
+
+        if user.role == "Admin":
+            return redirect(url_for("admin.dashboard"))
+        elif user.role == "Staff":
+            return redirect(url_for("staff.dashboard"))
+        else:
+            return redirect(url_for("trekker.dashboard"))
+
+    return render_template('auth/login.html')
+
+@auth.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        contact = request.form.get('contact')
+        password = request.form.get('password')
+
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash("Email already registered. Please log in.", "error")
+            return redirect(url_for("auth.register"))
+
+        hashed_pass = generate_password_hash(password)
+        new_trekker = User(name=name,
+                           email=email,
+                           contact=contact,
+                           role='Trekker',
+                           password=hashed_pass,
+                           is_blacklisted=False
+                           )
+        
+        db.session.add(new_trekker)
+        db.session.commit()
+        flash("Registration successful! Please log in.", "success")
+        return redirect(url_for("auth.login"))
+    
+    return render_template('auth/register.html')
+
+@auth.route("/dashboard")
+def dashboard():
+    if session.get('role') == 'Admin':
+        return redirect(url_for('admin.dashboard'))
+    elif session.get('role') == 'Staff':
+        return redirect(url_for('staff.dashboard'))
+    else:
+        return redirect(url_for('trekker.dashboard'))
+
+@auth.route("/logout")
+def logout():
+    session.clear()
+    flash("Logged out successfully.", "success")
+    return redirect(url_for("auth.login"))
+
