460a8328692b3017605235ca52fc67c687540c90
24f1000135 <24f1000135@ds.study.iitm.ac.in>
2026-07-13 11:42:03 +0530

    TMA-CM-5: Fixed status column in trekking history table by joining Trek table with Booking

--- trekker.py
+++ trekker.py
@@ -30,7 +30,8 @@ def dashboard():
 
     available_treks = query.all()
     booked_treks = Booking.query.filter_by(user_id=session['user_id'], status="Booked").all()
-    trekking_history = Booking.query.filter(Booking.user_id==session['user_id'], Booking.status.in_(['Completed', 'Cancelled'])).all()
+    trekking_history = Booking.query.join(Trek).filter(Booking.user_id==session['user_id'], 
+                                        Booking.status.in_(['Completed', 'Cancelled'])).all()
 
     return render_template("trekker/dashboard.html", available_treks=available_treks,
                             booked_treks=booked_treks, 
